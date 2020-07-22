3b61745b8671341a4d49942505b741f9d36ac823
ahmedmeshref <a.meshref@alustudent.com>
2020-07-22 05:47:00 +0200

    Enable Cors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,7 +12,15 @@ QUESTIONS_PER_PAGE = 10
 def create_app(config_object='development'):
     # create and configure the app
     app = Flask(__name__)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(config_by_name[config_object])
+
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     with app.app_context():
         setup_db(app)
